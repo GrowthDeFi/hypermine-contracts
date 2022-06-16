f38d156502d77a65c37e6da34d325b91a00a3c39
g-defi <growthdefi.dev@gmail.com>
2022-06-16 16:08:48 -0500

    updated

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -12,37 +12,40 @@ module.exports = async function (deployer, network, addresses) {
   if (network == "mumbai") {
     const sacrificesTo = addresses[0];
 
-    await deployer.deploy(WBNB, "WBNB", "WBNB", 18);
-    const wbnb = await WBNB.deployed();
-
-    await deployer.deploy(MOR, "MOR", "MOR", 18);
-    const mor = await MOR.deployed();
-
-    await deployer.deploy(DAI, "DAI", "DAI", 18);
-    const dai = await DAI.deployed();
-
-    await deployer.deploy(USDC, "USDC", "USDC", 18);
-    const usdc = await USDC.deployed();
-
-    await deployer.deploy(BUSD, "BUSD", "BUSD", 18);
-    const busd = await BUSD.deployed();
-
-    await deployer.deploy(SAFE, "SAFE", "SAFE", 18);
-    const safe = await SAFE.deployed();
-
-    await deployer.deploy(MockOracle);
-    const oracle = await MockOracle.deployed();
-
-    await deployer.deploy(MockOraclePair, wbnb.address, busd.address);
-    const oracleLP = await MockOraclePair.deployed();
-
     await deployer.deploy(
       HmineSacrifice,
       sacrificesTo,
-      wbnb.address,
-      oracleLP.address,
-      oracle.address
+      "0x9174f7b1b2588Cf4b830010A82B68345219D2B54",
+      "0xf3d351cf746c9F41e30908432bC7A9Af22BCa65C",
+      "0xC1C89d94B7e36E657F55F5Ae5eaDDd049B8C3129"
     );
     const hmineSacrifice = await HmineSacrifice.deployed();
+    await hmineSacrifice.addSacToken(
+      "0xedAf86e68Ea4a67681d7116f24564e39968006Cf",
+      true,
+      "0x0000000000000000000000000000000000000000"
+    );
+
+    await hmineSacrifice.addSacToken(
+      "0x98D95eDc8FCfd17284A0F9f20FA75372D6282011",
+      true,
+      "0x0000000000000000000000000000000000000000"
+    );
+
+    await hmineSacrifice.addSacToken(
+      "0x1bA00eDf85bDA74b1B20dc11F133fe893007C8Df",
+      true,
+      "0x0000000000000000000000000000000000000000"
+    );
+    await hmineSacrifice.addSacToken(
+      "0xdBA305f84Fe1cc02bbc1B5185D3FdF1AF30e2C64",
+      true,
+      "0x0000000000000000000000000000000000000000"
+    );
+    await hmineSacrifice.addSacToken(
+      "0xCEEAc2ca17D32DF5EB00ddE5C695Cb172aA954b3",
+      true,
+      "0x0000000000000000000000000000000000000000"
+    );
   }
 };
